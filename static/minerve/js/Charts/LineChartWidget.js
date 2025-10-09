5441fedd6217a56929313ccff2b06a18291ef46a
Oillphyra <oillphyra@mailsta.sh>
2025-10-09 07:39:13 -0600

    Athena: -Working on GDL Admin

---
+++ static/minerve/js/Charts/LineChartWidget.js
@@ -0,0 +1,39 @@
+import {AbstractApp} from "/static/minerve/js/core/AbstractApp.js";
+
+export class LineChartWidget extends AbstractApp {
+    urls = {
+        "_api_prefix":"",
+        "source":"/path/to/chart/data"
+    }
+
+    settings = {
+        "chart_js": {
+            "scales": {
+                 "y": {
+                    "beginAtZero": true
+                 }
+            }
+        }
+    }
+
+    chart_js = false
+    chart_builder(res) {
+          this.chart_js = new Chart(this.canvas, {
+                type: 'line',
+                data: res.data,
+                options: this.settings["chart_js"]
+          });
+
+    }
+    start(url) {
+        this.generic_api_getreq(url, false, this.chart_builder.bind(this));
+    }
+    constructor(settings, urls, elements) {
+        super(settings, urls, elements);
+        $.extend(this.settings, settings);
+        $.extend(this.urls, urls);
+        this.canvas = $("<canvas></canvas>");
+    }
+
+}
+export default LineChartWidget;
